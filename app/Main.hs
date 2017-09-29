15ea526dfc0079ea60f2bf354f98032388f63e4b
Clemens <clemens.jonischkeit@in.tum.de>
2017-09-29 15:43:08 +0200

    added solution for non unique functions names
    The ubuntu libc6_2.24-9ubuntu2_amd64.so contains two function named fmemopen.
    This non uniqueness of function names could result in a more than 100%
    matching. To deal with this, best case match numbers now are calculated
    using Lists instead of Sets

--- app/Main.hs
+++ app/Main.hs
@@ -44,6 +44,9 @@ statsForGroup matches = (countUniqueCorrect, countCorrect, count - countCorrect,
           countCorrect = length correct
           count = length matches
 
+collectNames :: [Function] -> [String]
+collectNames funs = filter (not . null) $ map funname funs
+
 runDot, runStrict, runLoose, runInfo :: Options -> IO ()
 runDot opts = printDotFiles outdir
     =<< map normalize <$> getFromNuc (optSigNuc opts)
@@ -63,21 +66,18 @@ runLoose opt = do
     f2 <- f1 `par` (\x -> if optNormalize opt then map normalize x else x) <$> getFromNuc (optTarNuc opt)
     _ <- f2 `seq` when (optVerbose opt) (putStrLn $ "loaded " ++ show (length f1) ++ " functions from " ++ optSigNuc opt) 
     _ <- when (optVerbose opt) (putStrLn $ "loaded " ++ show (length f2) ++ " functions from " ++ optTarNuc opt)
+--    putStrLn $ "named f1: " ++ show (length $ filter (not . null . funname) f1)
+--    putStrLn $ "not unique f1: " ++ unwords (map head . filter ((<) 1 . length) . List.group . List.sort . filter (not . null) $ map funname f1)
+--    putStrLn $ "named f2: " ++ show (length $ filter (not . null . funname) f2)
     let callGra1 = buildCallGraph f1
     let callGra2 = callGra1 `par` buildCallGraph f2
-    let f2Names = Set.fromList
-            . map funname
-                $ filter
-                    (\x -> (optMin opt <= length (funblocks x))
-                        && (not . null $ funname x))
-                    f2
-    let possMatch = Set.fromList
-            . map funname
-                $ filter
-                    (\x -> (optMin opt <= length (funblocks x))
-                        && (not . null $ funname x)
-                        && (funname x `Set.member` f2Names))
-                    f1
+    let f2Names = Set.fromList $ collectNames f2
+    let f1Named = map (\ a -> (length $ funblocks a, funname a)) $ filter (not . null . funname) f1
+    let allMatch = filter (flip Set.member f2Names . snd) f1Named
+--    putStrLn $ "unique names f1: " ++ show (length f1Names)
+--    putStrLn $ "unique names f2: " ++ show (length f2Names)
+    let possMatch = filter ((<=) (optMin opt) . fst) allMatch
+
     let groups =
             reverse . dropWhile ((>) (optMin opt) . (\ (x,_,_) -> x))
                 . mapMaybe
@@ -91,7 +91,7 @@ runLoose opt = do
     let stats = map (second statsForGroup) named_match
     
     _ <- when (optVerbose opt) $ mapM_ (\ (a,b) -> putStrLn $ "nodes: " ++ show a ++ ": " ++ show b) stats
-    _ <- putStrLn . ((++) "Total: " . show) . (\ (a,b,c) -> (a,b,c, length possMatch)) $ foldr (\ (_,(a1,b1,c1,_)) (a2,b2,c2) -> (a1 + a2, b1 + b2, c1 + c2)) (0,0,0) stats
+    _ <- putStrLn . ((++) "Total: " . show) . (\ (a,b,c) -> (a,b,c, length possMatch, length allMatch)) $ foldr (\ (_,(a1,b1,c1,_)) (a2,b2,c2) -> (a1 + a2, b1 + b2, c1 + c2)) (0,0,0) stats
 
     when (not $ optSkipPrint opt) . putStrLn . unlines . withStrategy (parListChunk 50 rseq) . map (\ x -> getFunName (fst x) ++ " -> " ++ unwords (map getFunName $ snd x)) 
         $ concatMap snd matched
